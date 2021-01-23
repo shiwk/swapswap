f96a33e308f885eb1f26251039a1b0f44604a4f1
shiwk <shiwktju@gmail.com>
2021-01-23 15:58:47 +0800

    Fetch aelf tx result.

--- elf/aelf_helper.js
+++ elf/aelf_helper.js
@@ -1,17 +1,17 @@
 async function pollMining(aelf, transactionId) {
     console.log(`>> Waiting for ${transactionId} the transaction to be mined.`);
 
+    await new Promise(resolve => setTimeout(resolve, 2000))
+        .catch(function () {
+            console.log("Promise Rejected");
+        });
+
     const currentResult = await aelf.chain.getTxResult(transactionId);
 
     if (currentResult.Status !== 'PENDING') {
         console.log('Tx Result:\n', currentResult);
         return currentResult;
     }
-
-    await new Promise(resolve => setTimeout(resolve, 2000))
-        .catch(function () {
-            console.log("Promise Rejected");
-        });
 
     await pollMining(aelf, transactionId);
 }
