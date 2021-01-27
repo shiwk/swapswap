c136b50dbbe97b71c19d832e2fe9a11c531f844c
shiwk <shiwktju@gmail.com>
2021-01-27 15:43:14 +0800

    SSC draw script.

--- sscDraw.js
+++ sscDraw.js
@@ -1,5 +1,4 @@
 const ssc = require('./elf/ssc');
-const ENV = require('./env');
 
 async function getCurrentPeriod() {
     return await ssc.getCurrentPeriod().catch(console.log);
@@ -18,18 +17,10 @@ async function draw(period) {
 }
 
 (async () => {
-    while (true) {
-        let time = new Date();
-        let s = time.getSeconds();
-        let m = time.getMinutes();
-        if (m % 10 === 0 && s === 0)
-        {
-            let latestPeriod = await getLatestDrawPeriod();
-            let currentPeriod = await getCurrentPeriod();
-            await prepareDraw();
-            let period = currentPeriod.periodNumber === 1 ? 1 : Number(latestPeriod.periodNumber) + 1;
-            await draw(period);
-            console.log('Draw done.');
-        }
-    }
+    let latestPeriod = await getLatestDrawPeriod();
+    let currentPeriod = await getCurrentPeriod();
+    await prepareDraw();
+    let period = currentPeriod.periodNumber === 1 ? 1 : Number(latestPeriod.periodNumber) + 1;
+    await draw(period);
+    console.log('Draw done.');
 })();
